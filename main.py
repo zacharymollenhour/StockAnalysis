91842287984eadc6504e9f71cda0d399ac4023d5
Zachary Mollenhour <42816677+zacharymollenhour@users.noreply.github.com>
2020-10-28 08:13:54 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+Zachary Mollenhour
+Stock Analysis Program
+This program will predict stock closing prices by utilizing a simple machine learning model
+Wihtin the program consists a GUI for the user to interact with
+The algoirthm will pull the required data from Yahoo finance API
+'''
+
